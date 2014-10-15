17054d2fbc4db1f3fe9296ea3bdef9d3f98c7955
Jason Tian <jason.tian@i-tea.com.tw>
2014-10-15 12:20:09 +0800

    validation and error msg

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,19 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+	
 	def new
 	end
 	
 	def create 
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to @article
-		
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end				
 	end
 	
 	def show
